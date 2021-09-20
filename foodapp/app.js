ffe16736f2c09a36aee970c5d7cd3f1ce50d55ad
Irashad <irshad30101999@gmail.com>
2021-09-21 00:34:05 +0530

    first commit

---
+++ foodapp/app.js
@@ -0,0 +1,38 @@
+
+const express = require('express');
+
+//server creation
+const app= express();
+let port = '8080';
+
+app.listen(port , function(){
+
+    console.log(`server is listening on port  ${port} `);
+});
+// types of request -> get post put delete
+
+app.get('/' , (req , res)=>{
+
+    console.log(req.hostname);
+    console.log(req.path);
+    console.log(req.method);
+    
+
+   res.send('<h1> hello  from backend</h1>');
+  // res.end();//for ending response 
+
+});
+
+let obj ={
+   'name': 'Irshad'
+}
+
+app.get('/user' , (req , res)=>{
+    console.log('users');
+     res.send(obj);
+});
+
+app.get('/home' , (req , res)=>{
+    console.log('users');
+     res.sendFile('./views/index.html' , {root:__dirname});
+});
